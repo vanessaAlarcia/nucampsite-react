1a65d059859a492230d3754b5ef72d6d8fbf5d8b
Vanessa Alarcia <vcalarcia@gmail.com>
2021-11-18 22:43:43 -0800

    Header and Footer

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -16,23 +16,22 @@ function RenderDirectoryItem({campsite, onClick}) {
             );
         }
 
-        function Directory(props) {
-
-            const directory = props.campsites.map(campsite => {
-                return (
-                    <div key={campsite.id} className="col-md-5 m-1">
-                        <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
-                    </div>
-                );
-            });
-
+function Directory(props) {
+    const directory = props.campsites.map(campsite => {
         return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
+}
 
 export default Directory;
